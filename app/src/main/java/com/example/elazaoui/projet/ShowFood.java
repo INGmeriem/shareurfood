c79de5d68e042ed557b2d08cea4306d82ff49805
Hoang-Bao-Duy NGUYEN <duy3105@gmail.com>
2016-03-09 13:04:36 +0100

    Update showfood layout and class

--- app/src/main/java/com/example/elazaoui/projet/ShowFood.java
+++ app/src/main/java/com/example/elazaoui/projet/ShowFood.java
@@ -1,7 +1,29 @@
 package com.example.elazaoui.projet;
 
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 /**
  * Created by DUYNGUYEN on 3/9/2016.
  */
-public class ShowFood {
+public class ShowFood extends ActionBarActivity {
+
+    ListView mListView;
+    String[] foods = new String[]{
+            "Hamburger", "Pizza", "Coca cola", "Ice tea", "Kebab", "Pho"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_showfood);
+
+        mListView = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(ShowFood.this, android.R.layout.simple_list_item_1, foods);
+
+
+    }
 }
