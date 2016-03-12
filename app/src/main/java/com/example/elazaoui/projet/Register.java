35f81104bbb2bb8410095bc5ebe0840b91a2a92a
Hoang-Bao-Duy NGUYEN <duy3105@gmail.com>
2016-03-12 23:01:40 +0100

    Update Register.java for new version

--- app/src/main/java/com/example/elazaoui/projet/Register.java
+++ app/src/main/java/com/example/elazaoui/projet/Register.java
@@ -4,13 +4,181 @@ package com.example.elazaoui.projet;
  * Created by elazaoui on 02/03/16.
  */
 
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.support.v7.app.AppCompatActivity;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.EditText;
+public class Register extends Activity implements OnClickListener{
+
+    private EditText user, pass, name, age, address, postalCode, email, phone;
+    private Button  mRegister;
+
+    // Progress Dialog
+    private ProgressDialog pDialog;
+
+    // JSON parser class
+    JSONParser jsonParser = new JSONParser();
+
+    //php login script
+
+    //localhost :
+    //testing on your device
+    //put your local ip instead,  on windows, run CMD > ipconfig
+    //or in mac's terminal type ifconfig and look for the ip under en0 or en1
+    // private static final String LOGIN_URL = "http://xxx.xxx.x.x:1234/webservice/register.php";
+
+    //testing on Emulator:
+    private static final String REGISTER_URL = "http://shareurfood.nguyenhoangbaoduy.info/register.php";
+
+    //testing from a real server:
+    //private static final String LOGIN_URL = "http://www.yourdomain.com/webservice/register.php";
+
+    //ids
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        user = (EditText)findViewById(R.id.etUsernameR);
+        pass = (EditText)findViewById(R.id.etPasswordR);
+        name = (EditText)findViewById(R.id.etName);
+        age = (EditText)findViewById(R.id.etAge);
+        address = (EditText)findViewById(R.id.etAddress);
+        postalCode = (EditText)findViewById(R.id.etPostalCode);
+        email = (EditText)findViewById(R.id.etEmail);
+        phone = (EditText)findViewById(R.id.etPhone);
+
+        mRegister = (Button)findViewById(R.id.bRegister);
+        mRegister.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+
+        String username = user.getText().toString();
+        String password = pass.getText().toString();
+        String nameU = name.getText().toString();
+        String ageU = age.getText().toString();
+        String addressU = address.getText().toString();
+        String postalCodeU = postalCode.getText().toString();
+        String emailU = email.getText().toString();
+        String phoneU = phone.getText().toString();
+
+        new CreateUser().execute(username, password, nameU, ageU, addressU, postalCodeU, emailU, phoneU);
+
+    }
+
+    class CreateUser extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        boolean failure = false;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(Register.this);
+            pDialog.setMessage("Creating User...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            // TODO Auto-generated method stub
+            // Check for success tag
+            int success;
+            String username = args[0];
+            String password = args[1];
+            String name = args[2];
+            String age = args[3];
+            String address = args[4];
+            String postalcode = args[5];
+            String email = args[6];
+            String phone = args[7];
+
+            try {
+                // Building Parameters
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("username", username));
+                params.add(new BasicNameValuePair("password", password));
+                params.add(new BasicNameValuePair("name", name));
+                params.add(new BasicNameValuePair("age", age));
+                params.add(new BasicNameValuePair("address", address));
+                params.add(new BasicNameValuePair("postalcode", postalcode));
+                params.add(new BasicNameValuePair("email", email));
+                params.add(new BasicNameValuePair("phone", phone));
+
+                Log.d("request!", "starting");
+
+                //Posting user data to script
+                JSONObject json = jsonParser.makeHttpRequest(
+                        REGISTER_URL, "POST", params);
+
+                // full json response
+                Log.d("Register attempt", json.toString());
+
+                // json success element
+                success = json.getInt(TAG_SUCCESS);
+                if (success == 1) {
+                    Log.d("User Created!", json.toString());
+                    finish();
+                    return json.getString(TAG_MESSAGE);
+                }else{
+                    Log.d("Register Failure!", json.getString(TAG_MESSAGE));
+                    return json.getString(TAG_MESSAGE);
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once product deleted
+            pDialog.dismiss();
+            if (file_url != null){
+                Toast.makeText(Register.this, file_url, Toast.LENGTH_LONG).show();
+            }
+
+        }
+
+    }
+
+}
+
+/*import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class Register extends AppCompatActivity implements View.OnClickListener{
@@ -60,4 +228,4 @@ public class Register extends AppCompatActivity implements View.OnClickListener{
             }
         });
     }
-}
+}*/
