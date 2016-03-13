3b485cacddf02fa78e16ea1a45e36db5cd908351
Hoang-Bao-Duy NGUYEN <duy3105@gmail.com>
2016-03-13 03:14:03 +0100

    Update changes Home.java

--- app/src/main/java/com/example/elazaoui/projet/Home.java
+++ app/src/main/java/com/example/elazaoui/projet/Home.java
@@ -4,30 +4,17 @@ package com.example.elazaoui.projet;
  * Created by elazaoui on 02/03/16.
  */
 
-
-import android.app.ProgressDialog;
 import android.content.Intent;;
 import android.content.SharedPreferences;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
-import android.widget.Toast;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class Home extends BaseActivity implements OnClickListener {
